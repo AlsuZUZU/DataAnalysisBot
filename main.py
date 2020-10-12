3d755dc2b7f0d6d94f33da822fb1f3196ed3473f
LAPTOP-BDF6NRED\Алсу Ситдикова <alsusik143139@mail.ru>
2020-10-12 16:21:07 +0300

    Главный коммит

---
+++ main.py
@@ -0,0 +1,27 @@
+import telebot
+#import config
+
+"""
+    Код добавлен главным программистом Zuzu Aidarovna
+    Кто удалит, будет казнён гвидо ван росумом
+"""
+
+bot = telebot.TeleBot('1027337352:AAHfDMuVygTgw39-oU1YVsFkSz3PJL6GSOA')
+
+
+@bot.message_handler(commands=['start'])  
+def start_command(message):  
+    bot.send_message(  
+        message.chat.id,  
+        'Приветствую, мы собираем статистику,\n' +
+        'Я помогу тебе с выбором преметов в этом семестре\n' +  
+        'Если тебе нужно узнать обо мне подробнее? - /help.'  )
+
+#bot.polling(none_stop=True, interval=5) 
+bot.polling(none_stop=True, interval= 3) 
+
+@bot.message_handler(commands=['help'])  
+def help_command(message):   
+    bot.send_message(  
+        message.chat.id,  'help here'
+    )
